7aeed631b4d5a5bc1008c10f962429af3ed0925d
EdsonPerycless <90483485+EdsonPerycless@users.noreply.github.com>
2022-09-18 00:41:54 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function padTo2Digits(num) {
+    return num.toString().padStart(2, '0');
+  }
+  
+  function formatDate(date) {
+    return [
+      padTo2Digits(date.getDate()),
+      padTo2Digits(date.getMonth() + 1),
+      date.getFullYear(),
+    ].join('/');
+  }
+
+let uptodate = new Date (document.lastModified);
+let element = document.querySelector('.date');
+element.style.color = 'white'
+element.textContent += `Last updated: ${formatDate(uptodate)} ${uptodate.getHours() === 0 ? uptodate.getHours() + '0' : uptodate.getHours() }:${uptodate.getMinutes()}:${uptodate.getSeconds()}`;
+console.log(element);
